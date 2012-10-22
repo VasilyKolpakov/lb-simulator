4cffac8b0643c6ba9439a20ab6f1b95fd7f96af7
Vasily Kolpakov <vasilykolpakov@gmail.com>
2012-10-22 14:21:49 +0400

    readable exception messages in ClassInjector

--- src/main/scala/ru/vasily/di/ClassInjector.scala
+++ src/main/scala/ru/vasily/di/ClassInjector.scala
@@ -8,23 +8,39 @@ class ClassInjector[T](val typeName: String, clazz: Class[T], dependenciesKeys: 
     val constructor = clazz.getDeclaredConstructors.apply(0)
       .asInstanceOf[Constructor[T]]
     val typesList = constructor.getParameterTypes.toList
+    if (dependenciesKeys.size != typesList.size) {
+      throw new ClassInjectorException(
+        "wrong nuber of dependencies: %d not %d".format(dependenciesKeys.size, typesList.size)
+      )
+    }
 
-    def newInstance(keyList: List[String], typeList: List[Class[_]], dependencies: List[Any] = Nil): (T, Map[String, Any]) = {
-      val key :: restKeys = keyList
-      val clazz :: restTypes = typeList
-      val monad: ObjectMonad[Any] = env.apply(key, clazz.asInstanceOf[Class[Any]])
-      if (restKeys.isEmpty) {
+    def newInstance(keyClassList: List[(String, Class[_])], dependencies: List[Any] = Nil): (T, Map[String, Any]) = {
+      val (key, clazz) :: rest = keyClassList
+      val monad = env.apply(key, clazz.asInstanceOf[Class[Any]])
+      if (rest.isEmpty) {
         monad.map(dep => {
           val constructorArgs = (dep :: dependencies).map(_.asInstanceOf[Object]).reverse
           constructor.newInstance(constructorArgs: _*)
         })
       } else {
-        monad.flatMap(dep => newInstance(restKeys, restTypes, dep :: dependencies))
+        monad.flatMap(dep => newInstance(rest, dep :: dependencies))
       }
     }
-
-    newInstance(dependenciesKeys, typesList)
+    try {
+      val keyClassList = dependenciesKeys.zip(typesList)
+      if (typesList.isEmpty) {
+        (constructor.newInstance(), Map())
+      } else {
+        newInstance(keyClassList)
+      }
+    } catch {
+      case e: ClassInjectorException => throw e
+      case x => throw new ClassInjectorException("error during instantiating " + typeName, x)
+    }
   }
+
+  class ClassInjectorException(message: String = "", cause: Throwable = null)
+    extends RuntimeException("error during instantiating " + typeName + ": " + message, cause)
 
 }
 
@@ -32,3 +48,4 @@ object ClassInjector {
   def apply[T](typeName: String, clazz: Class[T], dependenciesKeys: String*) =
     new ClassInjector(typeName, clazz, dependenciesKeys.toList)
 }
+
