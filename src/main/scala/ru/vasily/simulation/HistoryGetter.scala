9e35a4632ab1ab19a8dd68f41288e70c1c7649c9
Vasily Kolpakov <vasilykolpakov@gmail.com>
2012-11-14 10:10:05 +0400

    SimulationRunner refactor

---
+++ src/main/scala/ru/vasily/simulation/HistoryGetter.scala
@@ -0,0 +1,32 @@
+package ru.vasily.simulation
+
+object HistoryGetter {
+  def getHistory(clusterModel: ClusterModel, taskGenerator: TasksGenerator) = {
+    val initialMessagesReceiver = clusterModel.initialMessagesReceiver
+    val agents = clusterModel.agents
+    val tasks = taskGenerator.generateTasks
+    val taskMessages = for (task <- tasks) yield {
+      val taskMessage = Message(task, initialMessagesReceiver)
+      DelayedMessage(taskMessage, task.arrivalTime)
+    }
+
+    val initialModelState = ModelState(agents, taskMessages)
+
+    //    for (state <- initialModelState.nextStates) {
+    //      println(ModelState.prettyToString(state))
+    //    }
+
+    val lastModelState = initialModelState.nextStates.last
+    val history = lastModelState.agents.get(MonitoringService).collect {
+      case serviceState: MonitoringService.State => serviceState.serversHistory.toList.collect {
+        case (server: SimpleServer, taskRecords) => (server, taskRecords)
+      }.toMap
+    }.getOrElse {
+      throw new RuntimeException("MonitoringService is not present in cluster model " + clusterModel)
+    }
+
+    val totalSimulationTime = lastModelState.timeOfLastEvent
+
+    (totalSimulationTime, history)
+  }
+}
--- src/main/scala/ru/vasily/simulation/SimulationRunner.scala
+++ src/main/scala/ru/vasily/simulation/SimulationRunner.scala
@@ -8,32 +8,8 @@ class SimulationRunner(clusterModel: ClusterModel, taskGenerator: TasksGenerator
     Serializer.marshal(result)
   }
 
-
   def getNonSerializedResult: SimulationResult = {
-    val initialMessagesReceiver = clusterModel.initialMessagesReceiver
-    val agents = clusterModel.agents
-    val tasks = taskGenerator.generateTasks
-    val taskMessages = for (task <- tasks) yield {
-      val taskMessage = Message(task, initialMessagesReceiver)
-      DelayedMessage(taskMessage, task.arrivalTime)
-    }
-
-    val initialModelState = ModelState(agents, taskMessages)
-
-    //    for (state <- initialModelState.nextStates) {
-    //      println(ModelState.prettyToString(state))
-    //    }
-
-    val lastModelState = initialModelState.nextStates.last
-    val history = lastModelState.agents.get(MonitoringService).collect {
-      case serviceState: MonitoringService.State => serviceState.serversHistory.toList.collect {
-        case (server: SimpleServer, taskRecords) => (server, taskRecords)
-      }.toMap
-    }.getOrElse {
-      throw new RuntimeException("MonitoringService is not present in cluster model " + clusterModel)
-    }
-
-    val totalSimulationTime = lastModelState.timeOfLastEvent
+    val (totalSimulationTime, history) = HistoryGetter.getHistory(clusterModel, taskGenerator)
     val metrics = new AlgorithmMetrics(history, totalSimulationTime)
     val performanceMetrics = Map(
       "average slowdown" -> metrics.averageSlowdownMetric,
