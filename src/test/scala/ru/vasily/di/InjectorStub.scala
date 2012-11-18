4f6939a78fc9b71027a6eeeec30c70c8bbf41122
Vasily Kolpakov <vasilykolpakov@gmail.com>
2012-11-18 15:16:16 +0400

    InjectorStub handles no-dependency case

--- src/test/scala/ru/vasily/di/InjectorStub.scala
+++ src/test/scala/ru/vasily/di/InjectorStub.scala
@@ -2,22 +2,9 @@ package ru.vasily.di
 
 case class InjectorStub(dependencies: Seq[String], typeName: String = "Stub") extends Injector[Map[String, Any]] {
   def create(env: Environment) = {
-    val depList = dependencies.toList
-    def create(depList: List[String], depInstancesList: List[Any] = Nil): (Map[String, Any], Map[String, Any]) = {
-      val dep :: restDeps = depList
-      val monad = env(dep, classOf[Any])
-      if (restDeps.isEmpty) {
-        monad.map {
-          depInst =>
-            val depInstances = depInst :: depInstancesList
-            dependencies.zip(depInstances.reverse).toMap
-        }
-      } else {
-        monad.flatMap(depInst => create(restDeps, depInst :: depInstancesList))
-      }
-    }
-
-    create(depList)
+    val (instances, configs) = dependencies.map(env.getValueWithConfig(_, classOf[Any])).unzip
+    def createMap[T](seq: Seq[T]) = dependencies.zip(seq).toMap
+    (createMap(instances), createMap(configs))
   }
 
 }
